cd663439f3a223798a84a7f857cb2ce6a4a1c911
Abdul Samad <62374784+mr-loop-1@users.noreply.github.com>
2022-03-30 22:03:05 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2');
 
 var conn = mysql.createConnection({         //createConnection is fixed, con is variable
-  host: "localhost",
-  user: "root",
-  password: "yesmysql123",
-  database: "metro3"
+  host: "localhost",            // CHANGE IF DIFFERENT HOST
+  user: "root",                 // CHANGE IF DIFFERENT USER
+  password: " ",                // YOUR PASSWORD HERE
+  database: "metro3"            // CHANGE IF DATABASE NAME DIFFERENT
 });
 
 conn.connect(function(err) {                 //connect is fixed function to connect to con
