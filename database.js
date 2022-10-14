a5e52c3de1635892da37d4905fc8bcf51e3191fe
mr-loop-1 <loopolecreator1@gmail.com>
2022-10-14 13:25:30 +0530

    usersES6

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var conn = mysql.createConnection({         //createConnection is fixed, con is variable
   host: "localhost",            // CHANGE IF DIFFERENT HOST
   user: "root",                 // CHANGE IF DIFFERENT USER
-  password: " ",                // YOUR PASSWORD HERE
+  password: "yesmysql123",                // YOUR PASSWORD HERE
   database: "metro3"            // CHANGE IF DATABASE NAME DIFFERENT
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,152 +1,152 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../database');
+const express = require('express');
+const router = express.Router();
+const db = require('../database');
 
-var variabled;
-var variabled2;
-var time;
-var price;
+let variabled, variabled2, time, price;
 
-router.get('/metro', function(req, res, next) {
+router.get('/metro', (req, res) => {
     res.render("metro");
 });
-router.get("/about", function(req,res){
+router.get("/about", (req,res) => {
     res.render("about");
 });
-router.get("/planner", function(req,res){
+router.get("/planner", (req,res) => {
     res.render("planner");
 });
-router.get("/login_admin", function(req,res){
+router.get("/login_admin", (req,res) => {
     res.render("login_admin");
-  });
-  router.get("/contact", function(req, res){
+});
+router.get("/contact", (req, res) => {
     res.render('contact');
-  });
-
-  router.get('/team', function(req, res){
+});
+router.get('/team', (req, res) => {
     res.render('team');
-  });
-  
-  router.get('/portfolio', function(req, res){
+});  
+router.get('/portfolio', (req, res) => {
     res.render('portfolio');
-  });
-  
-  router.get('/networkmap', function(req, res){
+});  
+router.get('/networkmap', (req, res) => {
     res.render('networkmap');
-  });
+});
   
 
-router.post("/details", function(req, res, next){
+
+router.post("/details", (req, res) => {
     variabled=req.body.selectpicker;
     variabled2=req.body.selectpicker2;
     res.redirect('/users/details');
 });
 
-var gate1;
-var gate2;
-router.get('/details', function(req, res, next) {
+let gate1, gate2;
+
+router.get('/details', (req, res) => {
     db.query("TRUNCATE stack_Compare");
     db.query("TRUNCATE lastNodeTracker");
-
     db.query("SET max_sp_recursion_depth=100");
+
     gate1 = Math.floor((Math.random() * 4)+1);
     gate2 = Math.floor((Math.random() * 4)+6);
     
-    db.query("CALL insertMain(?,?)",[variabled,variabled2],function (err, results, fields){
+    db.query("CALL insertMain(?,?)",[variabled,variabled2], err => {
         if (err) throw err;
     });
 
-    db.query("SELECT COUNT(Station) AS aaa FROM stack_Compare", function(err, results, fields){
+    db.query("SELECT COUNT(Station) AS aaa FROM stack_Compare", (err, results) => {
         if (err) throw err;
         time = (10 * results[0].aaa)  - 5;
         price = (results[0].aaa*10);
     });
 
-    db.query("SELECT * FROM stack_Compare", function (err, results, fields) {
+    db.query("SELECT * FROM stack_Compare", (err, results) => {
         if (err) throw err;
-        console.log(results);
-        res.render('details', { title: 'User Listt', userdataa: results, timed: time, priced: price, enter: gate1, exit: gate2});
-  });
+        res.render("details", {
+            title: "User Listt",
+            userdataa: results,
+            timed: time,
+            priced: price,
+            enter: gate1,
+            exit: gate2,
+        });
+    });
+
 });
 
 
+let variabled3;
 
-
-
-
-
-var variabled3;
-
-router.get("/guest", function(req,res){
-    console.log(variabled3);
-    res.render("guest", {title: 'mains', start: variabled, end: variabled2, priced: price, id: variabled3, fname:fname, lname:lname});
+router.get("/guest", (req,res) => {
+    res.render("guest", {
+        title: "mains",
+        start: variabled,
+        end: variabled2,
+        priced: price,
+        id: variabled3,
+        fname: fname,
+        lname: lname,
+    });
     variabled3 = undefined;
 });
 
-var fname;
-var lname;
-var phone;
-var date;
+let fname, lname, phone, date;
 
 
-router.post("/status", function(req, res){
+router.post("/status", (req, res) => {
     fname=req.body.fname;
     lname=req.body.lname;
     phone=req.body.phone;
     date=req.body.myDate;
-    console.log(variabled3);
+
     variabled3 = Math.floor((Math.random() * 10000000) + 1);
-    console.log(variabled3);
-    db.query("Insert INTO details VALUES (?,?,?,?,?,?,?)",[variabled3,fname,lname,phone,date, variabled, variabled2],function (err, results, fields){
-        if (err) throw err;
-    });
+
+    db.query(
+        "Insert INTO details VALUES (?,?,?,?,?,?,?)",
+        [variabled3, fname, lname, phone, date, variabled, variabled2],
+        (err) => {
+            if (err) throw err;
+        }
+    );
+
     res.redirect('/users/guest');
-    
 });
 
 
 
-
-router.get("/status", function(req,res){
-    
-    res.render("status", {title: 'number', name1: fname, name2: lname, id: variabled3});
+router.get("/status", (req,res) => {
+    res.render("status", {
+        title: "number",
+        name1: fname,
+        name2: lname,
+        id: variabled3,
+    });
 });
-module.exports = router;
 
 
+let variabled4;
 
-
-
-
-
-
-
-
-
-
-
-
-
-var variabled4;
-
-router.post("/confirm", function(req, res){
+router.post("/confirm", (req, res) => {
     variabled4 = req.body.ticket;
     res.redirect('/users/confirm');
 });
 
-router.get("/confirm",function(req,res){
-    db.query("SELECT Ticket,Fname,Lname,Phone,DATE_FORMAT(Booking,\'%Y-%m-%d\') AS Booking,starter,dest FROM details WHERE Ticket=?",[variabled4],function(err, results, fields){
-        if (err) throw err;
-        console.log(results[0]);
-        res.render('confirm', { title: 'ticket info', Ticket: results[0]});
-    });
+router.get("/confirm", (req,res) => {
+    db.query(
+        "SELECT Ticket,Fname,Lname,Phone,DATE_FORMAT(Booking,'%Y-%m-%d') AS Booking,starter,dest FROM details WHERE Ticket=?",
+        [variabled4],
+        (err, results) => {
+            if (err) throw err;
+            res.render("confirm", { title: "ticket info", Ticket: results[0] });
+        }
+    );
 });
 
-router.post("/login",function(req,res){
-    if (req.body.username=="Tommy Vercetti" && req.body.pass=="thisismyaccount") res.redirect('/users/login');
+router.post("/login", (req,res) => {
+    if (req.body.username=="Tommy Vercetti" && req.body.pass=="thisismyaccount") 
+        res.redirect('/users/login');
     res.redirect("/users/metro");
 });
 
-router.get("/login", function(req, res){
+router.get("/login", (req, res) => {
     res.render('login');
 });
+
+module.exports = router;
