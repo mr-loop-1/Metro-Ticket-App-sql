fef5056fde8d159dec3c519a4fc0d5579ea46e61
mr-loop-1 <loopolecreator1@gmail.com>
2022-12-21 10:24:26 +0530

    FEAT: db config from env

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,11 @@ module.exports = {
   development: {
     client: "mysql2",
     connection: {
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "yesmysql123",
-        database: "metro4",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "yesmysql123",
+        database: process.env.DB_DATABASE || "metro4",
     },
     migrations: {
         directory: path.join(__dirname, 'database/migrations'),
